07a95b7679067435ce7582492ce45412ca50dd56
Bernd Dorn <dobe@users.noreply.github.com>
2020-08-23 22:09:44 +0200

    switch to junit5 (#2)

--- src/test/java/cz/startnet/utils/pgdiff/loader/PgDumpLoaderTest.kt
+++ src/test/java/cz/startnet/utils/pgdiff/loader/PgDumpLoaderTest.kt
@@ -1,52 +1,23 @@
-/**
- * Copyright 2006 StartNet s.r.o.
- *
- * Distributed under MIT license
- */
 package cz.startnet.utils.pgdiff.loader
 
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.io.File
 
-/**
- * Tests for PgDiffLoader class.
- *
- * @author fordfrog
- */
-@RunWith(value = Parameterized::class)
-class PgDumpLoaderTest
-/**
- * Creates a new instance of PgDumpLoaderTest.
- *
- * @param fileIndex [.fileIndex]
- */(
-    /**
-     * Index of the file that should be tested.
-     */
-    private val fileIndex: Int
-) {
-    /**
-     * Runs single test.
-     */
-    @Test(timeout = 1000)
-    fun loadSchema() {
-        PgDumpLoader.loadDatabaseSchema(
-            javaClass.getResourceAsStream("schema_$fileIndex.sql"),
-            "UTF-8", false, false, false
-        )
-    }
+class PgDumpLoaderTest {
 
-    companion object {
-        /**
-         * Provides parameters for running the tests.
-         *
-         * @return parameters for the tests
-         */
-        @JvmStatic
-        @Parameterized.Parameters(name = "schema_{0}.sql")
-        fun parameters(): Collection<*> {
-            return (1..18).toList()
+    fun sqlFiles() = javaClass.getResourceAsStream("/loader_test_files")?.let {
+        it.reader().readLines().filter { it.endsWith(".sql") }
+    } ?: error("directory for test files not found")
+
+    @ParameterizedTest
+    @MethodSource("sqlFiles")
+    fun loadSchema(sqlFile: File) {
+        javaClass.getResourceAsStream("/loader_test_files/$sqlFile").use {
+            PgDumpLoader.loadDatabaseSchema(
+                it,
+                "UTF-8", false, false, false
+            )
         }
     }
 }
