de8e0ddddf27bf1fb4abcb9ed7010ebd110dbfa3
Bernd Dorn <dorn@lovelysystems.com>
2025-06-10 21:48:08 +0200

    add plugin management repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "apgdiff"
 
 // load the local properties file if it exists
